d1f2ab9a33db814ec12a4a5c4231c1db9c47c847
Herlan12 <musikgratissaja@gmail.com>
2016-02-10 07:22:17 -0800

    Create album-deric.js

---
+++ album-deric.js
@@ -0,0 +1,134 @@
+
+var myPlaylist = [
+{
+        mp3:'https://dl-web.dropbox.com/s/1mrxumprgj4gfip/Yong%20yuan%20shi%20ni.mp3?dl=0',
+  title:'Yong yuan shi ni ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2015/12/deric-wan-wen-zhao-lun-yong-yuan-shi-ni.html',
+        cover:'http://3.bp.blogspot.com/-vW2BoLdl_4I/Vhn6JCNqHNI/AAAAAAAADXM/26uJlwWxQKc/s320/Novel%2BCover%2B%25288%2529.jpg'
+  
+    },
+{
+        mp3:'https://dl-web.dropbox.com/s/q3exnq14tg5fhgb/Wo%20men%20zhi%20jian%20liang%20ge%20shi%20jie.mp3?dl=0',
+  title:'Wo men zhi jian liang ge shi jie ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2013/05/deric-wan-wo-men-ce-cien-liang-ke-se-cie.html',
+        cover:'http://3.bp.blogspot.com/-vW2BoLdl_4I/Vhn6JCNqHNI/AAAAAAAADXM/26uJlwWxQKc/s320/Novel%2BCover%2B%25288%2529.jpg'
+  
+    },	
+ {
+        mp3:'https://dl-web.dropbox.com/s/3dzha25effa9fgp/Wen%20zhao%20lun%20-%20Tian%20di%20qing%20yuan.mp3?dl=0',
+  title:'Tian di qing yuan ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2015/12/deric-wan-wen-zhao-lun-tian-di-qing-yuan.html',
+        cover:'http://4.bp.blogspot.com/-E3KNBqIeWXk/Vhn6DnUBS_I/AAAAAAAADWU/3Vg08SFl7CM/s320/Novel%2BCover%2B%25283%2529.jpg'
+  
+    },	
+	{
+        mp3:'https://dl-web.dropbox.com/s/k8z5neddm302ss5/Jie%20kou.mp3?dl=0',
+  title:'Jie kou ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2015/12/deric-wan-wen-zhao-lun-jie-kou.html',
+        cover:'http://2.bp.blogspot.com/-9YuBpvbjTJg/Vhn6GbgpXWI/AAAAAAAADW0/KR0FolXquag/s320/Novel%2BCover%2B%25284%2529.jpg'
+  
+    },
+ {
+        mp3:'https://dl-web.dropbox.com/s/a236r5v01wdiafk/Gan%20Xie%20ni.mp3?dl=0',
+  title:'Gan xie ni ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2016/01/deric-wan-gan-xie-ni.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },	
+ {
+        mp3:'https://dl-web.dropbox.com/s/396dou1ruefp235/Gan%20jue.mp3?dl=0',
+  title:'Gan Jiao ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2016/01/deric-wan-gan-jiao.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/xs3l5bu6tif1n9d/Nei%20Jiu.mp3?dl=0',
+  title:'Nei jiu ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2016/01/deric-wan-nei-jiu.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/ju7ln73vrk1mbo9/Wei%20wo%20shou%20hou.mp3?dl=0',
+  title:'Wei wo shou hou ',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.co.id/2016/01/deric-wan-wei-wo-shou-hou.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  	
+ {
+        mp3:'https://dl-web.dropbox.com/s/lcfrmjk2m9x7f5q/Wo%20ben%20shan%20liang.mp3?dl=0',
+  title:'Wo ben shan liang',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.com/2016/02/deric-wan-wo-ben-shan-liang.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },
+{
+        mp3:'https://dl-web.dropbox.com/s/d45zgfeh2q7fcry/Nan%20dao%20wo%20shen%20de%20neng%20wang%20ji%20ni.mp3?dl=0',
+  title:'Nan dao wo zhen de neng wang ji ni',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.com/2016/02/deric-wan-nan-dao-wo-zhen-de-neng-wang.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/8ev0gdwyir5fcrs/Shuo%20huang.mp3?dl=0',
+  title:'Shuo huang',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.com/2016/02/deric-wan-shuo-huang.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/vtxkr0z0h1qmf2w/Yi%20sheng%20de%20ai%20gei%20ni.mp3?dl=0',
+  title:'Yi sheng de ai ge ni',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.com/2016/02/deric-wan-yi-sheng-de-ai-ge-ni.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/i0aitbfustc3l8p/Huan%20neng%20ai%20wo%20duo%20jiu.mp3?dl=0',
+  title:'Hai neng ai wo duo jiu',
+        artist:'Deric Wan  ',
+        rating:4,
+  duration:'',
+        buy:'http://mp3-mandarin.blogspot.com/2016/02/deric-wan-hai-neng-ai-wo-duo-jiu.html',
+        cover:'http://2.bp.blogspot.com/-qjN38QfpWQ4/VmLyqgwBgFI/AAAAAAAADyI/XgnHzavnLDA/s320/Deric%2BWan.jpg'
+  
+    },	
+	
+	
+];
