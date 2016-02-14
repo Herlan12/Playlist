aa157f2c384ff1e9cc37bbaa2234e4635e65eadc
Herlan12 <musikgratissaja@gmail.com>
2016-02-14 05:51:28 -0800

    Create yuni15populer.js

---
+++ yuni15populer.js
@@ -0,0 +1,145 @@
+
+var myPlaylist = [
+
+ {
+        mp3:'https://dl-web.dropbox.com/s/nkpfy1wscldwitq/Cinta%20Yang%20Kumau%20.ogg?dl=0',
+  title:'Cinta Yang Kumau ',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkZzlVTGtQdzdpNWM',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },
+ {
+        mp3:'https://dl-web.dropbox.com/s/v9q1rb0hjfkt37k/Ku%20Takut%20Mencintaimu%20.ogg?dl=0',
+  title:'Ku Takut Mencintaimu',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkYnBVUkpvanBXNDg',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/j7m2l00di8owysl/Return%20of%20the%20Condor%20Heroes%20.ogg?dl=0',
+  title:'Return of the Condor Heroes',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkdkx6aExFaXZnUk0',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/7m9nx45547antn3/Widuri.ogg?dl=0',
+  title:'Widuri',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkMWNJMUEzMEhZRW8',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/4f9645kk2wfkxpn/Aku%20Begini%20Engkau%20Begitu%20.ogg?dl=0',
+  title:'Aku Begini Engkau Begitu ',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkaUdNV0Z0Qk80ZjQ',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/eptj07bhxo4pbec/Semalam%20Di%20Cianjur%20.ogg?dl=0',
+  title:'Semalam Di Cianjur',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkaGtUY3J4dS1idk0',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/bhgesrumjiwlgy2/Sepanjang%20Jalan%20Kenangan.ogg?dl=0',
+  title:'Sepanjang Jalan Kenangan ',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkTEc4d0J4eF9UalU',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/ua3e3xnnva9kgqa/Angin%20Malam%20.ogg?dl=0',
+  title:'Angin Malam  ',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkYVZ6MG11WUpiYzQ',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/xltmlzwy71qplm7/Tangan%20Tak%20Sampai%20.ogg?dl=0',
+  title:'Tangan Tak Sampai ',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkQXREMWx4cTFYUUE',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/rvm14meldhsbbk1/Jangan%20Ditanya%20.ogg?dl=0',
+  title:'Jangan Ditanya',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkcmFXbGxncHhtYVk',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/k4dti4sm4gy2ggr/Gubahanku%20.ogg?dl=0',
+  title:'Gubahanku',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkYjBsZUQtd3lfcXc',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },
+{
+        mp3:'https://dl-web.dropbox.com/s/yjtan45p45n0x1o/Aku%20Jatuh%20Cinta.ogg?dl=0',
+  title:'Aku Jatuh Cinta',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkTzBvVVhGNnZCVEU',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/bo4ypnwfjzrz8f3/Mawar%20Beduri%20.ogg?dl=0',
+  title:'Mawar Berduri ',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkX3MwajdRcXY5V2s',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+{
+        mp3:'https://dl-web.dropbox.com/s/mqa6nvnr1nqj215/Mimpi%20Sedih.ogg?dl=0',
+  title:'Mimpi Sedih',
+        artist:'Yuni Shara',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0BxbNE9kNBPMkZjRBa05tWXZXVEU',
+        cover:'https://4.bp.blogspot.com/-mm6kF7ja3bE/VsBcOghXjWI/AAAAAAAACZw/CLbAbEwyPLo/s320/yuni%2BShara.jpg'
+  
+    },	
+	
+];
