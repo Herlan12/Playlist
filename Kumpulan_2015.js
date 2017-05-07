9e288fefa233f6bc24217ebc419da33e460cacc2
Herlan12 <musikgratissaja@gmail.com>
2017-05-08 01:10:37 +0700

    Add files via upload

---
+++ Kumpulan_2015.js
@@ -0,0 +1,350 @@
+
+var myPlaylist = [
+{
+        mp3:'https://dl-web.dropbox.com/s/3hjzi05wctqd9rj/Tang%20gu%20-%20Shi%20Bu%20Shi%20wo%20dui%20ni%20bu%20gou%20hao.mp3?dl=0',
+  title:'Shi Bu Shi wo dui ni bu gou hao',
+        artist:'Tang gu',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692bldPZi10RVJoUjg',
+        cover:'http://3.bp.blogspot.com/-QMazZd8dFR4/VqOyGoCXjgI/AAAAAAAAELw/LSXSNkSPRF0/s1600/Shi%2BBu%2BShi%2Bwo%2Bdui%2Bni%2Bbu%2Bgou%2Bhao.jpg'
+      },
+{
+        mp3:'https://dl-web.dropbox.com/s/19udzotb10u6rpa/Tang%20gu%20-%20Wo%20zai%20qian%20shi%20yue%20le%20ni.mp3?dl=0',
+  title:'Wo zai qian shi yue le ni',
+        artist:'Tang gu',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Mk1meXJWUmViOXM',
+        cover:'http://3.bp.blogspot.com/-qaPpiRyAMiQ/VqOvmgHnAKI/AAAAAAAAELY/G6LlVUwxrus/s1600/Wo%2Bzai%2Bqian%2Bshi%2Byue%2Ble%2Bni.jpg'
+      },
+{
+        mp3:'https://dl-web.dropbox.com/s/lv098k4vc8zgpoh/Li%20Jian%20-%20Jia%20ru%20ai%20you%20tian%20yi.mp3?dl=0',
+  title:'Jia ru ai you tian yi',
+        artist:'Li Jian ',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Q0ItTktPUnFOZ2c',
+        cover:'http://2.bp.blogspot.com/-EYkLK0Qsv-4/VqOuIyx9YHI/AAAAAAAAELM/Uvy-KC8MyCA/s1600/Jia%2Bru%2Bai%2Byou%2Btian%2Byi.jpg'
+      },
+{
+        mp3:'https://dl-web.dropbox.com/s/9vdfwjaqqbniw9i/Chen%20Mei%20xi%20%20-%20Wang%20le%20shang%20tong.mp3?dl=0',
+  title:'Wang le shang tong',
+        artist:'Chen Mei xi',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692ZmNzUFZ4bWdzRUk',
+        cover:'http://3.bp.blogspot.com/-p3Mmi-U-Ywk/VqJHB6kX9sI/AAAAAAAAEKQ/s6PhL-UW9R8/s1600/xhen%2Bmei%2Bxi.jpg'
+      },
+{
+        mp3:'https://dl-web.dropbox.com/s/93anycfkcm2a0ht/Xiang%20xiang%20-%20Ya%20xian%20de%20mofa.mp3?dl=0',
+  title:'Ya xian de mofa',
+        artist:'Xiang xiang',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692aU5GcWg1QWItR0E',
+        cover:'http://3.bp.blogspot.com/-6EfwZwjdzQA/Vppq2IMSOGI/AAAAAAAAEHA/KWUZr72cmUM/s1600/Xiang%2Bxiang.jpg'
+  
+    },
+{
+        mp3:'https://dl-web.dropbox.com/s/idn2clez5xrlrtk/Guo%20Jin%20tong%20-%20Yu%20Zai%20Fei.mp3?dl=0',
+  title:'Yu Zai Fei',
+        artist:'Guo Jin tong',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Q1ZvZUxyYXFVakE',
+        cover:'http://1.bp.blogspot.com/-hRuv47sBOkA/Vpe70ZaWwhI/AAAAAAAAEGg/usjiFrRysZo/s200/Yu%2BZai%2BFei.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692LV9jZlQ2dkdYOTQ',
+  title:'Lian',
+        artist:'Guo Jin tong',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692LV9jZlQ2dkdYOTQ',
+        cover:'http://2.bp.blogspot.com/-1ZAOsJjqlM8/Vpe6j7ktWoI/AAAAAAAAEGU/x6J5G2JOfOc/s200/Lian.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692RlAwVkRnZzdyYkk',
+  title:'Wen',
+        artist:'Guo Jin tong',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692RlAwVkRnZzdyYkk',
+        cover:'http://2.bp.blogspot.com/-oXnlnOZd6cw/Vpe5bHfUnqI/AAAAAAAAEGI/JZrXeNvBzm4/s1600/wen.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692UkhhU0k0dloxZjQ',
+  title:'Mei Hua Yu',
+        artist:'Guo Jin tong',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692UkhhU0k0dloxZjQ',
+        cover:'http://3.bp.blogspot.com/-T-3ywhgb19M/Vpe3tDZVX2I/AAAAAAAAEF8/LWUVDHNimjw/s200/Mei%2Bhua.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692elVmU2liS3ZWMVE',
+  title:'Wo ye ceng shen shen aizhe ni ',
+        artist:'Bao lin & Qiao Ling Er',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692elVmU2liS3ZWMVE',
+        cover:'http://3.bp.blogspot.com/-h0U9xCvdLO0/VpPjQaE4CjI/AAAAAAAAEEk/EuokgbSAhAM/s1600/Bao%2Blin%2B%2526%2BQiao%2BLing%2BEr.jpg'
+  
+    },
+ {
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692dVFyVVY0NzlvcUU',
+  title:'Wo Men De Gu Shi ',
+        artist:'Meng meng da tian tuan',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692dVFyVVY0NzlvcUU',
+        cover:'http://1.bp.blogspot.com/-0FvKJ8TVMBU/VpNzC-xkhhI/AAAAAAAAEEQ/Xs28yHRTOmI/s1600/Meng%2Bmeng%2Bda%2Btian%2Btuan%2B-%2BWo%2BMen%2BDe%2BGu%2BShi.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692YUNqYloxLWlaX2M',
+  title:'Di yi di xie  ',
+        artist:'Zhang Xiao Jun',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692YUNqYloxLWlaX2M',
+        cover:'http://2.bp.blogspot.com/-dAzkZL4yBRY/Voqoujw7-tI/AAAAAAAAECc/vVOgRf33GHk/s400/Zhang%2BXiao%2BJun.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Zmx6QThRNTFDb1U',
+  title:'Yu zhong de lian qu',
+        artist:'Xu Zi Ming',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Zmx6QThRNTFDb1U',
+        cover:'http://3.bp.blogspot.com/-17rd31TIlBo/Vok69-M4NOI/AAAAAAAAECE/tbFbWpLTs0M/s1600/Xu%2BZi%2BMing.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Sk02QTh0U1ltZ2c',
+  title:'Gu Nuan',
+        artist:'Si tu lan fang',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Sk02QTh0U1ltZ2c',
+        cover:'http://2.bp.blogspot.com/-KY61zZUtoG0/VoNtdlPkg5I/AAAAAAAAEB0/YImfTRy5RiE/s1600/S%25C4%25AB%2Btu%2Blan%2Bfang.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692dGNWaUIzbG1qVEU',
+  title:'Yang Guang Gu Niang',
+        artist:'Shao Bing',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692dGNWaUIzbG1qVEU',
+        cover:'http://1.bp.blogspot.com/-ZskxpR06ggI/VoNrs5WUUAI/AAAAAAAAEBo/E9BgjlguJkg/s1600/Shao%2BBing.jpg'
+  
+    },
+{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692TUJIdFotMDZGR1E',
+  title:'Ting zhe qing ge xiang zhe ni',
+        artist:'Liu zhen',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692TUJIdFotMDZGR1E',
+        cover:'http://4.bp.blogspot.com/-nKnOA-WWHd4/VoNprFwnkyI/AAAAAAAAEBc/FDRGOsbOdv4/s1600/Liu%2Bzhen.jpg'
+  
+    },
+
+ {
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692THFnczBPd29BR0k',
+  title:'Tong ai',
+        artist:'Yun fei fei',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692THFnczBPd29BR0k',
+        cover:'http://2.bp.blogspot.com/-kF_KCwpz2EY/VoKQV8Pka_I/AAAAAAAAEBE/gLDH0mmNkRo/s1600/Yun%2BFei%2BFei.jpg'
+  
+    },
+	 {
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692VjBvejZydW1HeVk',
+  title:'Wo de chun wen bu dao wo ai de ren',
+        artist:'Wang Yi Xin',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692VjBvejZydW1HeVk',
+        cover:'http://3.bp.blogspot.com/-0da0DL2Ju2Q/VoKOt2pWumI/AAAAAAAAEA4/Dx2JDhPW5yg/s1600/Wang%2BYi%2BXin.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Z080QjdreEl0bFk',
+  title:'Ni shi wo de  tian ',
+        artist:'Meng meng da tian tuan',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692Z080QjdreEl0bFk',
+        cover:'http://1.bp.blogspot.com/-Ahb9HTV3atA/VoEdJApN1jI/AAAAAAAAEAo/B2PrYIZV38o/s200/Meng%2Bmeng%2Bda%2Btian%2Btuan%2B-%2BNi%2Bshi%2Bwo%2Bde%2B%2Btian.jpg'
+  
+    },
+	 {
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692TmtlOURtYVFMSWc',
+  title:'Ming zhong zhu ding ',
+        artist:'Meng meng da tian tuan',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692TmtlOURtYVFMSWc',
+        cover:'http://3.bp.blogspot.com/-eKqpkC6Jovs/VnfzURxY-3I/AAAAAAAAD_g/SJ7rmR04PdU/s200/folder.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692a3QxdjlVUWk4OTA',
+  title:'Ai ni ai de wo xiang ge xiao san',
+        artist:'Xue wu ying',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692a3QxdjlVUWk4OTA',
+        cover:'http://3.bp.blogspot.com/-wKB_I9sR5Yc/VndslMcG_aI/AAAAAAAAD_M/4Fd6iPhpaLY/s200/Xue%2Bwu%2Bying.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692cmZzZjk0MDhVSEE',
+  title:'He ni hen xiang de ren',
+        artist:'Wang he zheng',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692cmZzZjk0MDhVSEE',
+        cover:'http://2.bp.blogspot.com/-fuhoZNJq-N4/VnWAR6EUe8I/AAAAAAAAD-8/PjcM3frcgLk/s200/Wang%2Bhe%2Bzheng.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692ckhjNWQ3b2pHMWM',
+  title:'Man yue  ',
+        artist:'Man yue',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692ckhjNWQ3b2pHMWM',
+        cover:'http://1.bp.blogspot.com/-tDL_jcs3sXc/VnV7k_zuC_I/AAAAAAAAD-s/i69wTvkQ_4c/s1600/chen%2Bsi%2Bsi.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692ektpVkVUeDVWM2s',
+  title:'Yi lei de tong lei  ',
+        artist:'Cai jian ya',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692ektpVkVUeDVWM2s',
+        cover:'http://1.bp.blogspot.com/-exClWTJXqd0/VnV5HdIFqGI/AAAAAAAAD-g/calc0ajO3_I/s200/Cai%2Bjian%2Bya.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692bW5fYm1TcVhzS0k',
+  title:'Jiu shi zhe me ren xing',
+        artist:'Wang lin & Yang hai biao ',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692bW5fYm1TcVhzS0k',
+        cover:'http://2.bp.blogspot.com/-66JgFpINLyU/VnLGjfOTxXI/AAAAAAAAD9E/C1eO9A8rVac/s200/Wang%2Blin%2B%2526%2BYang%2Bhai%2Bbiao%2B-%2Bjiushi%2Bzheme%2Brenxing.jpg'
+  
+    },
+	 {
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692UXBNT2Q5U3BTRTA',
+  title:'Wo ba gei wo mai le gao ya guo  ',
+        artist:'Lou qin ',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692UXBNT2Q5U3BTRTA',
+        cover:'http://1.bp.blogspot.com/-jJmdEKf4j5A/VnLEuvyhOBI/AAAAAAAAD88/L6tMmgY_908/s200/Lou%2Bqin%2B-%2BWo%2Bba%2Bgei%2Bwo%2Bmai%2Ble%2Bgao%2Bya%2Bguo.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692T3ZnNWtpQzZiWUU',
+  title:'Dong bei pian bei de di fang  ',
+        artist:'Yun fei fei',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692T3ZnNWtpQzZiWUU',
+        cover:'http://4.bp.blogspot.com/-cC8zXTb6vKo/VnLCeqAJK-I/AAAAAAAAD8w/tRF8jIVxmsg/s200/Yun%2Bfei%2Bfei%2B-%2BDong%2Bbei%2Bpian%2Bbei%2Bde%2Bdi%2Bfang.jpg'
+  
+    },
+	 {
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692MVBGOTJKVWY4eEk',
+  title:'Qin Shiming month ',
+        artist:'Meng meng da tian tuan',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692MVBGOTJKVWY4eEk',
+        cover:'http://4.bp.blogspot.com/-YnEhuCieZkg/Vm_tB_MB9mI/AAAAAAAAD74/Cyz_Ch1S5nk/s200/Meng%2Bmeng%2Bda%2Btian%2Btuan%2B-%2BQin%2BShiming%2Bmonth.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692ek1rNC1EaEFzSjQ',
+  title:'Lai bu jí shuo ai ni ',
+        artist:'Shao Yuhan  ',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692ek1rNC1EaEFzSjQ',
+        cover:'http://4.bp.blogspot.com/-hmnkE8tDIjg/Vm_p8hRfbFI/AAAAAAAAD7s/zGXK2p6n8Ic/s200/Shao%2BYuhan.jpg'
+  
+    },
+	 {
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692bnBSVnZtaVFRVWM',
+  title:'This Thing Called Love',
+        artist:'Maggie Fu ',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692bnBSVnZtaVFRVWM',
+        cover:'http://1.bp.blogspot.com/-P0WpjAolVCw/Vmli6BJmgAI/AAAAAAAAD4I/JQjAAvKNr5Q/s1600/This%2BThing%2BCalled%2BLove.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692RzIzdS1fR1o0NFU',
+  title:'If Only',
+        artist:'JJ Lin Jun Jie',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692RzIzdS1fR1o0NFU',
+        cover:'http://3.bp.blogspot.com/-fKLVtp3mOHs/VmlgEQjT7iI/AAAAAAAAD38/AnmtDJJh0H8/s1600/JJ%2BLin%2B%25E2%2580%2593%2BIf%2BOnly.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692RnBxbWQtX1VPQjA',
+  title:'WBie Shuo Mei Ai Guo',
+        artist:'William Wei ',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692RnBxbWQtX1VPQjA',
+        cover:'http://1.bp.blogspot.com/-BWXXLC8vcls/VmkRdztr4aI/AAAAAAAAD3o/rn5tpkNztzk/s1600/William%2BWei_e.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692dWJ1dldlYnkxR1k',
+  title:'jie',
+        artist:'Hebe Tian Fu Zen',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692dWJ1dldlYnkxR1k',
+        cover:'http://3.bp.blogspot.com/-oDYT2IKb0Ys/VmgpIfrRj6I/AAAAAAAAD2o/UV2M2BpsDns/s200/Hebe-Tien.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692cDdFS2xDNmllWnM',
+  title:'Xiao Xing Yun',
+        artist:'Hebe Tian Fu Zen',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692cDdFS2xDNmllWnM',
+        cover:'http://3.bp.blogspot.com/-oDYT2IKb0Ys/VmgpIfrRj6I/AAAAAAAAD2o/UV2M2BpsDns/s200/Hebe-Tien.jpg'
+  
+    },
+	{
+        mp3:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692cDdFS2xDNmllWnM',
+  title:'Far Away',
+        artist:'Cyndi Wang',
+        rating:4,
+  duration:'',
+        buy:'https://docs.google.com/uc?export=download&id=0ByaqFsu8M692cDdFS2xDNmllWnM',
+        cover:'http://2.bp.blogspot.com/-3tAPs8zj6-o/VmgVEthYjJI/AAAAAAAAD2I/YGb322QStm8/s1600/Cyndi%2BWang%2B-%2BFar%2BAway.jpg'
+  
+    },
+];
